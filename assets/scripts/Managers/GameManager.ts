245fd240ce7396113740e8c1197bb27901a3c18d
MagnaDrake <MagnaDrake@live.com>
2024-06-29 23:53:37 +0700

    import new textures

--- assets/scripts/Managers/GameManager.ts
+++ assets/scripts/Managers/GameManager.ts
@@ -1,22 +1,22 @@
 import {
-  _decorator,
-  Component,
-  Game,
-  instantiate,
-  KeyCode,
-  Node,
-  Prefab,
+	_decorator,
+	Component,
+	Game,
+	instantiate,
+	KeyCode,
+	Node,
+	Prefab,
 } from "cc";
 import {
-  Direction,
-  getDirection,
-  getRotationFromDirection,
-  IPoint,
-  RotateDirection,
-  VECTOR_DOWN,
-  VECTOR_LEFT,
-  VECTOR_RIGHT,
-  VECTOR_UP,
+	Direction,
+	getDirection,
+	getRotationFromDirection,
+	IPoint,
+	RotateDirection,
+	VECTOR_DOWN,
+	VECTOR_LEFT,
+	VECTOR_RIGHT,
+	VECTOR_UP,
 } from "../interfaces/IPoint";
 import { GridManager } from "./GridManager";
 import { Player } from "../objects/Player";
@@ -32,239 +32,241 @@ const { ccclass, property } = _decorator;
 
 @ccclass("GameManager")
 export class GameManager extends Component {
-  static _instance: GameManager;
+	static _instance: GameManager;
 
-  public static get Instance(): GameManager | undefined {
-    const instance = this._instance;
-    if (instance?.isValid) return instance;
-  }
+	public static get Instance(): GameManager | undefined {
+		const instance = this._instance;
+		if (instance?.isValid) return instance;
+	}
 
-  public static set Instance(value: GameManager) {
-    this._instance = value;
-  }
+	public static set Instance(value: GameManager) {
+		this._instance = value;
+	}
 
-  protected onLoad(): void {
-    const instance = GameManager.Instance;
+	protected onLoad(): void {
+		const instance = GameManager.Instance;
 
-    if (instance?.isValid && instance !== this) {
-      this.destroy();
-      return;
-    }
+		if (instance?.isValid && instance !== this) {
+			this.destroy();
+			return;
+		}
 
-    if (!instance || !instance.isValid) {
-      GameManager.Instance = this;
-    }
-  }
+		if (!instance || !instance.isValid) {
+			GameManager.Instance = this;
+		}
+	}
 
-  @property(Prefab)
-  playerPrefab: Prefab;
+	@property(Prefab)
+	playerPrefab: Prefab;
 
-  @property(Prefab)
-  emitterPrefab: Prefab;
+	@property(Prefab)
+	emitterPrefab: Prefab;
 
-  @property(Node)
-  gameWinScren: Node;
+	@property(Node)
+	gameWinScren: Node;
 
-  player: Player;
+	player: Player;
 
-  hasShownWin = false;
+	hasShownWin = false;
 
-  start() {
-    this.scheduleOnce(() => {
-      this.loadLevelData(0);
-    }, 0.2);
-  }
+	start() {
+		this.scheduleOnce(() => {
+			this.loadLevelData(0);
+		}, 0.2);
+	}
 
-  onUndoKeyInput() {
-    CommandManager.Instance.undoCommandBatch();
-    GridManager.Instance.updateGridState();
-    // should not have to check for win if its undoing
-  }
+	onUndoKeyInput() {
+		CommandManager.Instance.undoCommandBatch();
+		GridManager.Instance.updateGridState();
+		// should not have to check for win if its undoing
+	}
 
-  onInteractInput(keyCode: KeyCode) {
-    switch (keyCode) {
-      case KeyCode.KEY_Q:
-        this.rotateEntityOnGrid(RotateDirection.COUNTER_CLOCKWISE);
-        return;
-      case KeyCode.KEY_E:
-        this.rotateEntityOnGrid(RotateDirection.CLOCKWISE);
-        return;
-    }
-  }
+	onInteractInput(keyCode: KeyCode) {
+		switch (keyCode) {
+			case KeyCode.KEY_Q:
+				this.rotateEntityOnGrid(RotateDirection.COUNTER_CLOCKWISE);
+				return;
+			case KeyCode.KEY_E:
+				this.rotateEntityOnGrid(RotateDirection.CLOCKWISE);
+				return;
+		}
+	}
 
-  rotateEntityOnGrid(rot: RotateDirection) {
-    const targetPos = this.player.getPosInFront();
-    const targetTile = GridManager.Instance.getTileInGrid(
-      targetPos.x,
-      targetPos.y
-    );
+	rotateEntityOnGrid(rot: RotateDirection) {
+		const targetPos = this.player.getPosInFront();
+		const targetTile = GridManager.Instance.getTileInGrid(
+			targetPos.x,
+			targetPos.y
+		);
 
-    if (!targetTile) return;
+		if (!targetTile) return;
 
-    // currently assuming a tile can only have 1 entity
-    const batch = new CommandBatch();
-    const entityToRotate = targetTile.entities[0];
-    if (!entityToRotate) return;
-    const rotateEntityCommand = new StepRotationCommand(entityToRotate, rot);
-    batch.add(rotateEntityCommand);
-    this.executeCommand(batch);
-  }
+		// currently assuming a tile can only have 1 entity
+		const batch = new CommandBatch();
+		const entityToRotate = targetTile.entities[0];
+		if (!entityToRotate) return;
+		const rotateEntityCommand = new StepRotationCommand(
+			entityToRotate,
+			rot
+		);
+		batch.add(rotateEntityCommand);
+		this.executeCommand(batch);
+	}
 
-  onMovementKeyInput(keyCode: KeyCode) {
-    let direction;
-    switch (keyCode) {
-      case KeyCode.ARROW_UP:
-        direction = VECTOR_UP;
-        break;
-      case KeyCode.ARROW_DOWN:
-        direction = VECTOR_DOWN;
-        break;
-      case KeyCode.ARROW_LEFT:
-        direction = VECTOR_LEFT;
-        break;
-      case KeyCode.ARROW_RIGHT:
-        direction = VECTOR_RIGHT;
-        break;
-    }
-    // jank
-    // surely there is a better way to handle movement logic
-    // will change later if needed.
+	onMovementKeyInput(keyCode: KeyCode) {
+		let direction;
+		switch (keyCode) {
+			case KeyCode.ARROW_UP:
+				direction = VECTOR_UP;
+				break;
+			case KeyCode.ARROW_DOWN:
+				direction = VECTOR_DOWN;
+				break;
+			case KeyCode.ARROW_LEFT:
+				direction = VECTOR_LEFT;
+				break;
+			case KeyCode.ARROW_RIGHT:
+				direction = VECTOR_RIGHT;
+				break;
+		}
+		// jank
+		// surely there is a better way to handle movement logic
+		// will change later if needed.
 
-    // movement rules
-    // attempt to move towards a direction
-    // if player is currently does not face the input direction, rotate towards it
-    // check if tile if traversable or not. if not, do not progress further
-    // check if traversable alreacy contains an entity
-    // if the player initially does not face the entity, stop
-    //
+		// movement rules
+		// attempt to move towards a direction
+		// if player is currently does not face the input direction, rotate towards it
+		// check if tile if traversable or not. if not, do not progress further
+		// check if traversable alreacy contains an entity
+		// if the player initially does not face the entity, stop
+		//
 
-    let changeDirection = false;
-    const targetPosition: IPoint = {
-      x: this.player.position.x + direction.x,
-      y: this.player.position.y + direction.y,
-    };
+		let changeDirection = false;
+		const targetPosition: IPoint = {
+			x: this.player.position.x + direction.x,
+			y: this.player.position.y + direction.y,
+		};
 
-    // check if tile exist
-    const tile = GridManager.Instance.getTileInGrid(
-      targetPosition.x,
-      targetPosition.y
-    );
+		// check if tile exist
+		const tile = GridManager.Instance.getTileInGrid(
+			targetPosition.x,
+			targetPosition.y
+		);
 
-    const batch = new CommandBatch();
+		const batch = new CommandBatch();
 
-    const targetDirection = getDirection(direction.x, direction.y);
-    if (this.player.direction !== targetDirection) {
-      const rotatePlayerCommand = new RotateCommand(
-        this.player,
-        targetDirection
-      );
-      batch.add(rotatePlayerCommand);
-      changeDirection = true;
-    }
+		const targetDirection = getDirection(direction.x, direction.y);
+		if (this.player.direction !== targetDirection) {
+			const rotatePlayerCommand = new RotateCommand(
+				this.player,
+				targetDirection
+			);
+			batch.add(rotatePlayerCommand);
+			changeDirection = true;
+		}
 
-    if (tile) {
+		if (tile) {
+			if (!tile.traversable) {
+				this.executeCommand(batch);
+				return;
+			}
+			// also check if tile has any existing entity
+			// if another entity exist, check if pushing it to the same direction results a vali move
+			// for now assume tiles can only have one entity
+			console.log("check for entities on this tile");
+			//   console.log(tile.position);
+			//  console.log(tile.entities);
 
-      if(!tile.traversable){
-        this.executeCommand(batch)
-        return;
-      }
-      // also check if tile has any existing entity
-      // if another entity exist, check if pushing it to the same direction results a vali move
-      // for now assume tiles can only have one entity
-      console.log("check for entities on this tile");
-      //   console.log(tile.position);
-      //  console.log(tile.entities);
+			if (tile.entities.length > 0 && tile.entities[0] !== undefined) {
+				// console.log(tile.entities[0]);
 
-      if (tile.entities.length > 0 && tile.entities[0] !== undefined) {
-        // console.log(tile.entities[0]);
+				if (changeDirection) {
+					// console.log("only attempt to face the entity but not move towards");
+					this.executeCommand(batch);
+					return;
+				}
 
-        if (changeDirection) {
-          // console.log("only attempt to face the entity but not move towards");
-          this.executeCommand(batch);
-          return;
-        }
+				const nextTile = GridManager.Instance.getTileInGrid(
+					targetPosition.x + direction.x,
+					targetPosition.y + direction.y
+				);
 
-        const nextTile = GridManager.Instance.getTileInGrid(
-          targetPosition.x + direction.x,
-          targetPosition.y + direction.y
-        );
+				if (nextTile && nextTile.entities.length < 1) {
+					const moveEntityCommand = new MoveCommand(
+						tile.entities[0],
+						targetPosition.x + direction.x,
+						targetPosition.y + direction.y,
+						tile.entities[0].position.x,
+						tile.entities[0].position.y
+					);
+					batch.add(moveEntityCommand);
+					//console.log("will attempt to move entities");
+				} else {
+					//console.log("expected to hit a wall because of emitter cannot move");
+					this.executeCommand(batch);
+					return;
+				}
+			}
 
-        if (nextTile && nextTile.entities.length < 1) {
-          const moveEntityCommand = new MoveCommand(
-            tile.entities[0],
-            targetPosition.x + direction.x,
-            targetPosition.y + direction.y,
-            tile.entities[0].position.x,
-            tile.entities[0].position.y
-          );
-          batch.add(moveEntityCommand);
-          //console.log("will attempt to move entities");
-        } else {
-          //console.log("expected to hit a wall because of emitter cannot move");
-          this.executeCommand(batch);
-          return;
-        }
-      }
+			const movePlayerCommand = new MoveCommand(
+				this.player,
+				targetPosition.x,
+				targetPosition.y,
+				this.player.position.x,
+				this.player.position.y
+			);
 
-      const movePlayerCommand = new MoveCommand(
-        this.player,
-        targetPosition.x,
-        targetPosition.y,
-        this.player.position.x,
-        this.player.position.y
-      );
+			batch.add(movePlayerCommand);
+		}
+		this.executeCommand(batch);
+	}
 
-      batch.add(movePlayerCommand);
-    }
-    this.executeCommand(batch);
-  }
+	executeCommand(batch: CommandBatch) {
+		CommandManager.Instance.executeCommandBatch(batch);
+		// todo
+		// i dont really like this approach because this makes the command batch execution segmented
+		const win = GridManager.Instance.updateGridState();
+		console.log("has won", win);
 
-  executeCommand(batch: CommandBatch) {
-    CommandManager.Instance.executeCommandBatch(batch);
-    // todo
-    // i dont really like this approach because this makes the command batch execution segmented
-    const win = GridManager.Instance.updateGridState();
-    console.log("has won", win);
+		if (win) {
+			this.onWinLevel();
+		}
+	}
 
-    if (win) {
-      this.onWinLevel();
-    }
-  }
+	onWinLevel() {
+		console.log("win level");
+		if (this.hasShownWin) return;
+		this.gameWinScren.active = true;
+		this.hasShownWin = true;
+	}
 
-  onWinLevel() {
-    console.log("win level");
-    if (this.hasShownWin) return;
-    this.gameWinScren.active = true;
-    this.hasShownWin = true;
-  }
+	onRestartLevelKeyInput() {
+		// gotta think if its faster/easier to just reload the gameplay scene or undo all commands
+		// for now lets just reload gamescene
+		// on second thought maybe not
+		// flush all grid and entities and commands from memory
+		// then reinit the level
+		// might need object pooling in the future
+		GridManager.Instance.clearGrid();
+		CommandManager.Instance.clearCommands();
+		this.player.node.destroy();
+		this.player = undefined;
+		this.loadLevelData(0);
+		this.gameWinScren.active = false;
+	}
 
-  onRestartLevelKeyInput() {
-    // gotta think if its faster/easier to just reload the gameplay scene or undo all commands
-    // for now lets just reload gamescene
-    // on second thought maybe not
-    // flush all grid and entities and commands from memory
-    // then reinit the level
-    // might need object pooling in the future
-    GridManager.Instance.clearGrid();
-    CommandManager.Instance.clearCommands();
-    this.player.node.destroy();
-    this.player = undefined;
-    this.loadLevelData(0);
-    this.gameWinScren.active = false;
-  }
+	loadLevelData(id: number) {
+		const levelData = LevelManager.Instance.levelData[id];
+		if (levelData) {
+			GridManager.Instance.createLevel(levelData);
+		}
+	}
 
-  loadLevelData(id: number) {
-    const levelData = LevelManager.Instance.levelData[id];
-    if (levelData) {
-      GridManager.Instance.createLevel(levelData);
-    }
-  }
-
-  // protected update(dt: number): void {
-  //   const win = GridManager.Instance.updateGridState();
-  //   if (win) {
-  //     //console.log("has won", win);
-  //     this.onWinLevel();
-  //   }
-  // }
+	// protected update(dt: number): void {
+	//   const win = GridManager.Instance.updateGridState();
+	//   if (win) {
+	//     //console.log("has won", win);
+	//     this.onWinLevel();
+	//   }
+	// }
 }
--- assets/scripts/utils/LevelReader.ts
+++ assets/scripts/utils/LevelReader.ts
@@ -2,31 +2,31 @@ import { IPoint, Size } from "../interfaces/IPoint";
 import { EmitterTypes } from "../objects/Emitter";
 
 export enum TileTypeData {
-  Floor = 12,
-  Wall,
-  Panel = 43,
+	Floor = 12,
+	Wall,
+	Panel = 43,
 }
 
 export enum SplitterDataType {
-  // todo
-  // figure out a way to more cleanly separate obstacle types in the level editor
-  // for some reason class and type from the tiled map editor doesnt work
-  // simplest way is to have an enum that reads all individual GID
-  // then map the GID into specific objects
-  QUAD = 31,
+	// todo
+	// figure out a way to more cleanly separate obstacle types in the level editor
+	// for some reason class and type from the tiled map editor doesnt work
+	// simplest way is to have an enum that reads all individual GID
+	// then map the GID into specific objects
+	QUAD = 31,
 }
 
 export function getRotationOffset(rot: number) {
-  switch (rot) {
-    case 0:
-      return { x: 0, y: 0 };
-    case 90:
-      return { x: 0, y: -1 };
-    case 180:
-      return { x: -1, y: -1 };
-    case -90:
-      return { x: -1, y: 0 };
-  }
+	switch (rot) {
+		case 0:
+			return { x: 0, y: 0 };
+		case 90:
+			return { x: 0, y: -1 };
+		case 180:
+			return { x: -1, y: -1 };
+		case -90:
+			return { x: -1, y: 0 };
+	}
 }
 
 // probably dont need this?
@@ -34,158 +34,162 @@ export function getRotationOffset(rot: number) {
 // for now lump splitter into data class
 // perhaps in the future if we want to have a fixed/static/heavy splitter we will handle in the future
 export enum EmitterDataClass {
-  Basic,
-  Heavy,
-  Static,
-  Fixed,
-  Splitter,
+	Basic,
+	Heavy,
+	Static,
+	Fixed,
+	Splitter,
 }
 
 export interface EntityListData {
-  player: PlayerData;
-  emitters: EmitterData[];
+	player: PlayerData;
+	emitters: EmitterData[];
 }
 
 export interface EntityData {
-  position: IPoint;
-  rotation: number;
+	position: IPoint;
+	rotation: number;
 }
 
 export interface PlayerData extends EntityData {}
 
 export interface EmitterData extends EntityData {
-  outputType: string;
-  subtype: string;
+	outputType: string;
+	subtype: string;
 }
 
 export interface TileData {
-  position: IPoint;
-  type: TileTypeData;
+	position: IPoint;
+	type: TileTypeData;
 }
 
 export interface LevelData {
-  levelSize: Size;
-  terrain: TileData[];
-  entities: EntityListData;
+	levelSize: Size;
+	terrain: TileData[];
+	entities: EntityListData;
 }
 
 export enum LayerGroupName {
-  Terrain = "Terrain",
-  Entities = "Entities",
+	Terrain = "Terrain",
+	Entities = "Entities",
 }
 
 export enum EntityLayerName {
-  Player = "Player",
-  Emitters = "Emitters",
+	Player = "Player",
+	Emitters = "Emitters",
 }
 
 export function readRawLevelData(json: any) {
-  //definitly can optimize/abstract this even further
-  // do it later
+	//definitly can optimize/abstract this even further
+	// do it later
 
-  //console.log(json);
-  const { width, height, layers } = json;
-  //console.log("reading width and height");
-  //console.log(width, height);
+	//console.log(json);
+	const { width, height, layers } = json;
+	//console.log("reading width and height");
+	//console.log(width, height);
 
-  let terrainLayer;
-  let entityLayer;
+	let terrainLayer;
+	let entityLayer;
 
-  layers.forEach((layer) => {
-    const { name } = layer;
-    if (name === LayerGroupName.Terrain) {
-      terrainLayer = layer.layers[0].data;
-    } else if (name === LayerGroupName.Entities) {
-      entityLayer = layer.layers;
-    }
-  });
+	layers.forEach((layer) => {
+		const { name } = layer;
+		if (name === LayerGroupName.Terrain) {
+			terrainLayer = layer.layers[0].data;
+		} else if (name === LayerGroupName.Entities) {
+			entityLayer = layer.layers;
+		}
+	});
+	console.log("Terrain");
+	console.log(terrainLayer);
 
-  //  console.log(terrainLayer);
+	console.log(entityLayer);
 
-  //console.log(entityLayer);
+	let playerData;
+	let emitterData;
 
-  let playerData;
-  let emitterData;
+	for (let i = 0; i < entityLayer.length; i++) {
+		const layer = entityLayer[i];
+		const { name } = layer;
 
-  for (let i = 0; i < entityLayer.length; i++) {
-    const layer = entityLayer[i];
-    const { name } = layer;
+		if (name === EntityLayerName.Player) {
+			playerData = layer.objects[0];
+		} else if (name === EntityLayerName.Emitters) {
+			emitterData = layer.objects;
+		}
+	}
+	//console.log(playerData);
+	//console.log(emitterData);
 
-    if (name === EntityLayerName.Player) {
-      playerData = layer.objects[0];
-    } else if (name === EntityLayerName.Emitters) {
-      emitterData = layer.objects;
-    }
-  }
-  //console.log(playerData);
-  //console.log(emitterData);
+	let tileData: TileData[] = [];
+	let index = 0;
 
-  let tileData: TileData[] = [];
-  let index = 0;
+	for (let y = 0; y < height; y++) {
+		for (let x = 0; x < width; x++) {
+			const fixedY = Math.abs(y - 9);
+			const pos = { x: x, y: fixedY };
+			tileData.push({ position: pos, type: terrainLayer[index] });
+			index++;
+		}
+	}
 
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      const fixedY = Math.abs(y - 9);
-      const pos = { x: x, y: fixedY };
-      tileData.push({ position: pos, type: terrainLayer[index] });
-      index++;
-    }
-  }
+	let emitterParsedData = [];
 
-  let emitterParsedData = [];
+	emitterData.forEach((emitter) => {
+		// console.log(emitter.class);
+		//console.log(emitter.x, emitter.y);
+		const posRotOffset = getRotationOffset(emitter.rotation);
 
-  emitterData.forEach((emitter) => {
-    // console.log(emitter.class);
-    //console.log(emitter.x, emitter.y);
-    const posRotOffset = getRotationOffset(emitter.rotation);
+		const emitterPos: IPoint = {
+			x: emitter.x / 64 + posRotOffset.x,
+			y: Math.abs(emitter.y / 64 - 1 - (height - 1)) + posRotOffset.y,
+		};
 
-    const emitterPos: IPoint = {
-      x: emitter.x / 64 + posRotOffset.x,
-      y: Math.abs(emitter.y / 64 - 1 - (height - 1)) + posRotOffset.y,
-    };
+		console.log("read");
+		const emitterProps = emitter.properties;
 
-    console.log("read");
-    const emitterProps = emitter.properties;
+		const outputType = emitterProps[0].value;
+		const emitterType = emitterProps[1].value;
 
-    const outputType = emitterProps[0].value;
-    const emitterType = emitterProps[1].value;
+		const fixRot =
+			Math.abs(emitter.rotation) === 90
+				? -emitter.rotation
+				: emitter.rotation;
 
-    const fixRot =
-      Math.abs(emitter.rotation) === 90 ? -emitter.rotation : emitter.rotation;
+		const data: EmitterData = {
+			outputType: outputType,
+			position: emitterPos,
+			rotation: fixRot,
+			subtype: emitterType,
+		};
 
-    const data: EmitterData = {
-      outputType: outputType,
-      position: emitterPos,
-      rotation: fixRot,
-      subtype: emitterType,
-    };
+		emitterParsedData.push(data);
+	});
 
-    emitterParsedData.push(data);
-  });
+	const posRotOffset = getRotationOffset(playerData.rotation);
 
-  const posRotOffset = getRotationOffset(playerData.rotation);
+	const playerDataFixRot =
+		Math.abs(playerData.rotation) === 90
+			? -playerData.rotation
+			: playerData.rotation;
 
-  const playerDataFixRot =
-    Math.abs(playerData.rotation) === 90
-      ? -playerData.rotation
-      : playerData.rotation;
+	const entityListData: EntityListData = {
+		player: {
+			position: {
+				x: playerData.x / 64 + posRotOffset.x,
+				y:
+					Math.abs(playerData.y / 64 - 1 - (height - 1)) +
+					posRotOffset.y,
+			},
+			rotation: playerDataFixRot,
+		},
+		emitters: emitterParsedData,
+	};
 
-  const entityListData: EntityListData = {
-    player: {
-      position: {
-        x: playerData.x / 64 + posRotOffset.x,
-        y: Math.abs(playerData.y / 64 - 1 - (height - 1)) + posRotOffset.y,
-      },
-      rotation: playerDataFixRot,
-    },
-    emitters: emitterParsedData,
-  };
+	const levelData: LevelData = {
+		levelSize: { width, height },
+		terrain: tileData,
+		entities: entityListData,
+	};
 
-  const levelData: LevelData = {
-    levelSize: { width, height },
-    terrain: tileData,
-    entities: entityListData,
-  };
-
-  return levelData;
+	return levelData;
 }
