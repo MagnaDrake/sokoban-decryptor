a6987109f168eaa5f779096d3d305ddac4c21d2c
MagnaDrake <yudha.putra@shopee.com>
2024-07-01 19:05:46 +0700

    stub pause menu

---
+++ assets/scripts/Managers/VolumeController.ts
@@ -0,0 +1,58 @@
+import { _decorator, Component, Label, Node, Slider } from "cc";
+import { AudioManager } from "./AudioManager";
+const { ccclass, property } = _decorator;
+
+@ccclass("VolumeController")
+export class VolumeController extends Component {
+  @property(Slider)
+  bgmSlider!: Slider;
+  @property(Slider)
+  sfxSlider!: Slider;
+
+  @property(Label)
+  BGMVolumeLabel!: Label;
+
+  @property(Label)
+  SFXVolumeLabel!: Label;
+
+  masterVolume = 1;
+  sfxVolume = 1;
+
+  protected onLoad(): void {
+    this.masterVolume = AudioManager.Instance.masterVolume;
+    this.sfxVolume = AudioManager.Instance.sfxVolumeRatio;
+
+    this.bgmSlider.progress = this.masterVolume;
+    this.sfxSlider.progress = this.sfxVolume;
+
+    this.BGMVolumeLabel.string = parseInt(
+      (this.masterVolume * 100).toFixed(2)
+    ).toString();
+    this.SFXVolumeLabel.string = parseInt(
+      (this.sfxVolume * 100).toFixed(2)
+    ).toString();
+  }
+  onSliderChange(slider: Slider, id?: string) {
+    if (id === "bgm") {
+      this.adjustBGMVolume(slider.progress);
+    } else {
+      this.adjustSFXVolume(slider.progress);
+    }
+  }
+
+  adjustBGMVolume(value: number) {
+    this.masterVolume = value;
+
+    // what the fuck is this?
+    this.BGMVolumeLabel.string = parseInt((value * 100).toFixed(2)).toString();
+
+    AudioManager.Instance.adjustBGMVolume(value);
+  }
+
+  adjustSFXVolume(value: number) {
+    this.sfxVolume = value;
+    this.SFXVolumeLabel.string = parseInt((value * 100).toFixed(2)).toString();
+
+    AudioManager.Instance.adjustSFXVolume(value);
+  }
+}
