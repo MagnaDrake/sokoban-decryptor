9910338fcb6c3ed2963d2f6d638866f6bc28f814
MagnaDrake <yudha.putra@shopee.com>
2024-07-01 13:06:27 +0700

    animate movement

--- assets/scripts/utils/anim.ts
+++ assets/scripts/utils/anim.ts
@@ -1,29 +1,46 @@
 import {
-	Node,
-	view,
-	Vec3,
-	director,
-	tween,
-	easing,
-	randomRangeInt,
-	ITweenOption,
+  Node,
+  view,
+  Vec3,
+  director,
+  tween,
+  easing,
+  randomRangeInt,
+  ITweenOption,
 } from "cc";
 
-export function moveTo(
-	object: Node,
-	target: Vec3,
-	duration: number,
-	completeCallback?: any
-) {
-	const tweenProps = { worldPosition: target };
-	let onComplete;
-	if (completeCallback !== undefined) {
-		onComplete = completeCallback;
-	}
-	const easingProps: ITweenOption = { easing: easing.circOut, onComplete };
-	const moveTween = tween(object)
-		.to(duration, tweenProps, easingProps)
-		.start();
+export const FRAME = 0.016;
 
-	return moveTween;
+export function moveTo(
+  object: Node,
+  target: Vec3,
+  duration: number,
+  completeCallback?: any
+) {
+  const tweenProps = { worldPosition: target };
+  let onComplete;
+  if (completeCallback !== undefined) {
+    onComplete = completeCallback;
+  }
+  const easingProps: ITweenOption = { easing: easing.circOut, onComplete };
+  const moveTween = tween(object).to(duration, tweenProps, easingProps).start();
+
+  return moveTween;
+}
+
+export function moveToLocal(
+  object: Node,
+  target: Vec3,
+  duration: number,
+  completeCallback?: any
+) {
+  const tweenProps = { position: target };
+  let onComplete;
+  if (completeCallback !== undefined) {
+    onComplete = completeCallback;
+  }
+  const easingProps: ITweenOption = { easing: easing.circOut, onComplete };
+  const moveTween = tween(object).to(duration, tweenProps, easingProps).start();
+
+  return moveTween;
 }
