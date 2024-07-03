ad71e5b6ae25fd2d412991b8cf4831e5c546d260
MagnaDrake <MagnaDrake@live.com>
2024-07-03 15:32:21 +0700

    adjust encoder and decoder

--- assets/scripts/utils/bwt.ts
+++ assets/scripts/utils/bwt.ts
@@ -1,31 +1,26 @@
-const eof = "~";
+const eof = "z";
 
-function encode_cmp_func(x, y) {
+function encodeCompFunc(x, y) {
   //@ts-ignore
   return (x[1] > y[1]) - (x[1] < y[1]);
 }
 
-function decode_cmp_func(x, y) {
+function decodeCompFunc(x, y) {
   //@ts-ignore
-  //console.log(x,y)
   return (x > y) - (x < y);
 }
 
-//Takes text to be transformed and its length as arguments
-//and returns the corresponding suffix array
-
-function compute_suffix_array(input_text, len_text) {
+function getSuffixArray(input_text, len_text) {
   let suff = [];
   for (let i = 0; i < len_text; i++) {
     suff.push([i, input_text.slice(i)]);
   }
-  suff.sort(encode_cmp_func);
+  suff.sort(encodeCompFunc);
   let suffix_arr = suff.map((item) => item[0]);
   return suffix_arr;
 }
-// Takes suffix array and its size as arguments
-// and returns the Burrows-Wheeler Transform of given text
-function find_last_char(input_text, suffix_arr, n) {
+
+function findLast(input_text, suffix_arr, n) {
   let bwt_arr = "";
   for (let i = 0; i < n; i++) {
     let j = suffix_arr[i] - 1;
@@ -36,43 +31,33 @@ function find_last_char(input_text, suffix_arr, n) {
   }
   return bwt_arr;
 }
-// Driver program to test functions above
 
-/*
-function inverseBWT (string s)
-    create empty table
-    repeat length(s) times
-        // first insert creates first column
-        insert s as a column of table before first column of the table
-        sort rows of the table alphabetically
-    return (row that ends with the 'EOF' character)
-*/
 export function encodeBWT(input) {
   const append = input.concat(eof);
-  const suf = compute_suffix_array(append, append.length);
-  return find_last_char(append, suf, append.length);
+  console.log(append);
+  const suf = getSuffixArray(append, append.length);
+  return findLast(append, suf, append.length);
 }
 
 export function decodeBWT(input) {
   const bwtChars = input.split("");
-  console.log(bwtChars);
   const col = Array(input.length).fill("");
 
   for (let i = 0; i < input.length; i++) {
     for (let j = 0; j < input.length; j++) {
       let string = col[j];
       string = bwtChars[j].concat(string);
-      console.log(string);
       col[j] = string;
     }
-    col.sort(decode_cmp_func);
+    col.sort(decodeCompFunc);
   }
 
   let decode;
   for (let i = 0; i < col.length; i++) {
-    if (col[i].charAt(col[i].length - 1) === "~") console.log(col[i]);
-    decode = col[i];
+    if (col[i].charAt(col[i].length - 1) === eof) {
+      decode = col[i].slice(0, -1);
+    }
   }
 
-  return decode.slice(0, -1);
+  return decode;
 }
--- assets/scripts/utils/savedata.ts
+++ assets/scripts/utils/savedata.ts
@@ -1,6 +1,9 @@
 import { decodeBWT, encodeBWT } from "./bwt";
 import { decodeRLE, encodeRLE } from "./rle";
 
+const char = "abcdefghijklmnopqrstuvwxyz";
+const delimiter = "Z";
+
 export interface UserSaveData {
   completedLevels: number[];
   perfectLevels: number[];
@@ -9,14 +12,56 @@ export interface UserSaveData {
 export function encodeSaveData(data: string) {
   const bwt = encodeBWT(data);
   const rle = encodeRLE(bwt);
-
-  console.log(rle);
   return rle;
 }
 
 export function decodeSaveData(save: string) {
   const irle = decodeRLE(save);
   const ibwt = decodeBWT(irle);
-  console.log(ibwt);
   return ibwt;
+}
+
+export function convertStringToLevel(input) {
+  const save = input.split(delimiter);
+  for (let i = 0; i < save.length; i++) {
+    let entry = save[i];
+    let num = entry.split("");
+    let id = "";
+    for (let j = 0; j < num.length; j++) {
+      const conv = num[j].charCodeAt(0) - 96;
+      id = id.concat(conv.toString());
+    }
+    save[i] = parseInt(id);
+  }
+  return save;
+}
+
+export function convertLevelToString(input) {
+  const save = input.toString().split(",");
+  console.log(save);
+
+  for (let i = 0; i < save.length; i++) {
+    let entry = save[i];
+    let num = entry.split("");
+    console.log("split entries", num);
+    let id = "";
+    for (let j = 0; j < num.length; j++) {
+      console.log(
+        "get char from id",
+        num[j],
+        char.charAt(parseInt(num[j]) - 1)
+      );
+      id = id.concat(char.charAt(parseInt(num[j]) - 1));
+    }
+    console.log(id);
+    save[i] = id;
+  }
+
+  const regex = /,/g;
+
+  let res = save.toString().replace(regex, delimiter);
+
+  console.log(res);
+
+  return res;
 }
